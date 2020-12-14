8f099b8147412e93fbb7521a185059eb0a70c6ac
fauzan <fauzan@email.com>
2020-12-14 16:25:07 +0700

    implementasi reply message

--- public/index.php
+++ public/index.php
@@ -1,34 +1,49 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
  
+ 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+ 
  
 use \LINE\LINEBot;
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
 use \LINE\LINEBot\SignatureValidator as SignatureValidator;
  
+ 
 $pass_signature = true;
+ 
  
 // set LINE channel_access_token and channel_secret
 $channel_access_token = "cATNPUaqHUCcE2WtdLTNjUeQ1sl0v6ajjdIshsTYpqZzvp+XSyLOlqOAlELo6xc4L8YynWaIlm+6yTZZd2dppXNbKGbEjysoFvFvsRnrvjA3Y9MZY8em2EbqxouO3i5G+s1ZiBIUG8O0yTcLSmc4HQdB04t89/1O/w1cDnyilFU=";
 $channel_secret = "893b3644881ea464470ed920fae31e9d";
  
+ 
 // inisiasi objek bot
 $httpClient = new CurlHTTPClient($channel_access_token);
 $bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
  
+ 
+ 
+ 
 $app = AppFactory::create();
 $app->setBasePath("/public");
+ 
+ 
+ 
  
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello World!");
     return $response;
 });
+ 
  
 // buat route untuk webhook
 $app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
@@ -36,8 +51,10 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
     $body = $request->getBody();
     $signature = $request->getHeaderLine('HTTP_X_LINE_SIGNATURE');
  
+ 
     // log body and signature
     file_put_contents('php://stderr', 'Body: ' . $body);
+ 
  
     if ($pass_signature === false) {
         // is LINE_SIGNATURE exists in request header?
@@ -45,12 +62,14 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
             return $response->withStatus(400, 'Signature not set');
         }
  
+ 
         // is this request comes from LINE?
         if (!SignatureValidator::validateSignature($body, $channel_secret, $signature)) {
             return $response->withStatus(400, 'Invalid signature');
         }
     }
-    
+ 
+ 
     $data = json_decode($body, true);
     if(is_array($data['events'])){
         foreach ($data['events'] as $event)
@@ -62,9 +81,15 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
                     // send same message as reply to user
                     $result = $bot->replyText($event['replyToken'], $event['message']['text']);
  
+ 
+ 
+ 
                     // or we can use replyMessage() instead to send reply message
                     // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
                     // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+ 
+ 
+ 
  
                     $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
                     return $response
